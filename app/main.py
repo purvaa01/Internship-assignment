d042ba6475a95829a14a06a72062f48d180df66a
purvaa01 <purvaa462@gmail.com>
2026-07-02 21:42:23 +0530

    Add S3 file upload endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from datetime import datetime
+import boto3
 import socket
 
 app = FastAPI()
 
+BUCKET_NAME = "purva-internship-backup-2026"
+
+s3 = boto3.client("s3")
 @app.get("/")
 def home():
     return {
@@ -16,3 +20,22 @@ def health():
         "timestamp": datetime.utcnow().isoformat() + "Z"
     }
 
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+        s3.upload_fileobj(
+            file.file,
+            BUCKET_NAME,
+            file.filename
+        )
+
+        return {
+            "message": "File uploaded successfully",
+            "filename": file.filename
+        }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
